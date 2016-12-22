cd2a9b9c0c654526b5e33f6c031ec5b418677730
aditya2337 <aditya.n@gai.co.in>
2016-12-22 11:47:37 +0530

    removed the useless comments

--- Car.php
+++ Car.php
@@ -1,19 +1,5 @@
 <?php 
 
-/*for ($i = 5; $i < 0; $i--) { 
-  for ($j = 5; $j < 0 ; $j--) { 
-  	if ( $i == $j) {
-  	  echo "*";
-  	} else {
-  		echo "0";
-  	}
-  }
-  echo "<br>";
-} 
-echo "stri";
-echo "tag 2";
-*/
-
 namespace My\Project\CarObject;
 
 class Car {
